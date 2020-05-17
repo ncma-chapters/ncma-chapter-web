5f815229e54b7bdb9911510356cb9352a564fe61
Kelson Adams <kadams@governmentcio.com>
2020-05-17 17:28:08 -0600

    Add event api module with create event registration module

---
+++ src/templates/Event/api.js
@@ -0,0 +1,35 @@
+// Node modules.
+import get from 'lodash/get';
+// Relative imports.
+import request, { API_BASE_URL } from '../../utils/request';
+
+export const createEventRegistration = async (options = {}) => {
+  const response = await request(`${API_BASE_URL}/event-registrations`, {
+    body: JSON.stringify({
+      data: {
+        type: 'eventRegistrations',
+        attributes: {
+          data: {
+            company: get(options, 'company', ''),
+            email: get(options, 'email', ''),
+            firstName: get(options, 'firstName', ''),
+            lastName: get(options, 'lastName', ''),
+            title: get(options, 'title', ''),
+          },
+        },
+        relationships: {
+          ticketClass: {
+            data: {
+              type: 'ticketClasses',
+              id: get(options, 'ticketClassID', ''),
+            },
+          },
+        },
+      },
+    }),
+    method: 'POST',
+  });
+  console.log('response', response);
+
+  return response;
+};
