95829b41e3fdaefb960bc9281c76094d4c91f455
Kelson Adams <kadams@governmentcio.com>
2020-05-17 21:06:59 -0600

    Add events to footer

--- src/global/Footer/index.js
+++ src/global/Footer/index.js
@@ -4,7 +4,9 @@ import get from 'lodash/get';
 import map from 'lodash/map';
 import toUpper from 'lodash/toUpper';
 import trimEnd from 'lodash/trimEnd';
+import take from 'lodash/take';
 import { window } from 'browser-monads';
+import { StaticQuery, graphql } from 'gatsby';
 // Relative imports.
 import config from '../../config';
 import {
@@ -27,54 +29,69 @@ import {
   Wrapper,
 } from './styles';
 
-const Footer = () => {
-  // Derive config properties.
-  const chapterName = get(config, 'chapterName');
-  const email = get(config, 'contact.email');
-  // const formattedAddress = get(config, 'contact.formattedAddress');
-  // const formattedPhoneNumber = get(config, 'contact.formattedPhoneNumber');
-  const navItems = get(config, 'navItems');
-  // const phoneNumber = get(config, 'contact.phoneNumber');
-  // const socialLinks = get(config, 'socialLinks');
+const Footer = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        allEvents {
+          nodes {
+            id
+            name
+          }
+        }
+      }
+    `}
+    render={(data) => {
+      // Derive config properties.
+      const chapterName = get(config, 'chapterName');
+      const email = get(config, 'contact.email');
+      // const formattedAddress = get(config, 'contact.formattedAddress');
+      // const formattedPhoneNumber = get(config, 'contact.formattedPhoneNumber');
+      const navItems = get(config, 'navItems');
+      // const phoneNumber = get(config, 'contact.phoneNumber');
+      // const socialLinks = get(config, 'socialLinks');
 
-  // Derive the current year.
-  const currentYear = new Date().getFullYear();
+      // Derive the current year.
+      const currentYear = new Date().getFullYear();
 
-  // Derive the current pathname.
-  const pathname = trimEnd(get(window, 'location.pathname'), '/');
+      // Derive the current pathname.
+      const pathname = trimEnd(get(window, 'location.pathname'), '/');
 
-  return (
-    <Wrapper>
-      <MainFooter>
-        <Column>
-          {/* Logo */}
-          <StyledLogo />
+      // Derive events.
+      const events = get(data, 'allEvents.nodes');
 
-          {/* Nav Items */}
-          <FooterLinks>
-            {map(navItems, (item) => {
-              // Derive item properties.
-              const label = get(item, 'label');
-              const href = get(item, 'href');
+      return (
+        <Wrapper>
+          <MainFooter>
+            <Column>
+              {/* Logo */}
+              <StyledLogo />
 
-              return (
-                <FooterLink key={label} to={href} selected={pathname === trimEnd(href, '/')}>
-                  {toUpper(label)}
-                </FooterLink>
-              );
-            })}
-          </FooterLinks>
-        </Column>
+              {/* Nav Items */}
+              <FooterLinks>
+                {map(navItems, (item) => {
+                  // Derive item properties.
+                  const label = get(item, 'label');
+                  const href = get(item, 'href');
 
-        {/* Vertical Line Separator */}
-        <Line />
+                  return (
+                    <FooterLink key={label} to={href} selected={pathname === trimEnd(href, '/')}>
+                      {toUpper(label)}
+                    </FooterLink>
+                  );
+                })}
+              </FooterLinks>
+            </Column>
 
-        <Column>
-          {/* Header */}
-          <ColumnHeader>KEEP CONNECTED</ColumnHeader>
+            {/* Vertical Line Separator */}
+            <Line />
 
-          {/* Social Media Links */}
-          {/* <SocialLinks>
+            <Column>
+              {/* Header */}
+              <ColumnHeader>KEEP CONNECTED</ColumnHeader>
+
+              {/* Social Media Links */}
+              {/* <SocialLinks>
             {map(socialLinks, (link) => {
               // Derive social link properties.
               const label = get(link, 'label');
@@ -89,46 +106,50 @@ const Footer = () => {
             })}
           </SocialLinks> */}
 
-          {/* Contact Phone Number */}
-          {/* <FooterExternalLink href={`tel:${phoneNumber}`}>{formattedPhoneNumber}</FooterExternalLink> */}
+              {/* Contact Phone Number */}
+              {/* <FooterExternalLink href={`tel:${phoneNumber}`}>{formattedPhoneNumber}</FooterExternalLink> */}
 
-          {/* Contact Email */}
-          <FooterExternalLink href={`mailto:${email}`}>{email}</FooterExternalLink>
-        </Column>
+              {/* Contact Email */}
+              <FooterExternalLink href={`mailto:${email}`}>{email}</FooterExternalLink>
+            </Column>
 
-        {/* Vertical Line Separator */}
-        <Line />
+            {/* Vertical Line Separator */}
+            <Line />
 
-        <Column>
-          {/* Header */}
-          <ColumnHeader>EVENTS</ColumnHeader>
+            <Column>
+              {/* Header */}
+              <ColumnHeader>EVENTS</ColumnHeader>
 
-          {/* Event Links */}
-          {/* <FooterLinks>
-            <FooterLink to="/events">Industry Best Practices Seminar</FooterLink>
-            <FooterLink to="/events">Spring Chapter Meeting</FooterLink>
-            <FooterLink to="/events">Industry Standards Symposium</FooterLink>
-          </FooterLinks> */}
+              {/* Event Links */}
+              <FooterLinks>
+                {map(take(events, 3), (event) => (
+                  <FooterLink key={get(event, 'id')} to={`/events/${get(event, 'id', '')}`}>
+                    {get(event, 'name', '')}
+                  </FooterLink>
+                ))}
+              </FooterLinks>
 
-          {/* Events Link */}
-          {/* <FooterLink to="/events">See all events</FooterLink> */}
-        </Column>
-      </MainFooter>
+              {/* Events Link */}
+              {/* <FooterLink to="/events">See all events</FooterLink> */}
+            </Column>
+          </MainFooter>
 
-      <SubFooter>
-        <Copyright>
-          &copy; {currentYear} NCMA {chapterName}
-        </Copyright>
+          <SubFooter>
+            <Copyright>
+              &copy; {currentYear} NCMA {chapterName}
+            </Copyright>
 
-        {/* <Address>{formattedAddress}</Address> */}
+            {/* <Address>{formattedAddress}</Address> */}
 
-        <SubFooterLinks>
-          <SubFooterLink to="/privacy">Privacy Policy</SubFooterLink>
-          <SubFooterLink to="/terms-of-service">Terms of Service</SubFooterLink>
-        </SubFooterLinks>
-      </SubFooter>
-    </Wrapper>
-  );
-};
+            <SubFooterLinks>
+              <SubFooterLink to="/privacy">Privacy Policy</SubFooterLink>
+              <SubFooterLink to="/terms-of-service">Terms of Service</SubFooterLink>
+            </SubFooterLinks>
+          </SubFooter>
+        </Wrapper>
+      );
+    }}
+  />
+);
 
 export default Footer;
